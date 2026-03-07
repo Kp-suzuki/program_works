6f766b9cc663185e2e176e68caba28f1f6e4c53f
Kp-suzuki <keips999999@yahoo.co.jp>
2026-03-07 18:58:44 +0900

    Delete test_game directory

--- test_game/map/kusa.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="kusa" tilewidth="32" tileheight="32" tilecount="10" columns="2">
- <image source="../../ウディタ2_32x32mapchip_20210215/MapChip/[A]Grass1-Dirt1_pipo.png" width="64" height="160"/>
-</tileset>
--- test_game/map/water.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="water" tilewidth="32" tileheight="32" tilecount="40" columns="8">
- <image source="../../ウディタ2_32x32mapchip_20210215/MapChip/[A]Water2_pipo.png" width="256" height="160"/>
-</tileset>
